3299b81035c12331fa4d2da7dde619fdeea02793
Eduardo Lima <eduardo.lima@outlook.de>
2024-12-03 14:04:59 +0100

    Base filtered between Berlin and BB

--- R-GroupC.R
+++ R-GroupC.R
@@ -3,9 +3,10 @@ library(ggrepel)
 library(RColorBrewer)
 library(lubridate)
 
+#BASE
 # Define the URL of the csv.gz file and the destination file path
-url <- "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/tutorial/datascience2024/matsim_outputs/output-1pct/policy/berlin-v6.3.output_trips.csv.gz"
-destfile <- "yourfile.csv.gz"
+url <- "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/tutorial/datascience2024/matsim_outputs/output-1pct/base/berlin-v6.3.output_persons.csv.gz"
+destfile <- "berlin-v6.3.output_persons.csv.gz"
 
 # Download the file
 download.file(url, destfile)
@@ -18,4 +19,35 @@ file_dir <- dirname(destfile)
 setwd(file_dir)
 
 # Unzip and read the file
-trips_data <- read_delim(gzfile(basename(destfile)))
+base_data <- read_delim(gzfile(basename(destfile)))
+
+sample(base_data)
+
+#POLICY
+# Define the URL of the csv.gz file and the destination file path
+url <- "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/tutorial/datascience2024/matsim_outputs/output-1pct/policy/berlin-v6.3.output_persons.csv.gz"
+destfile <- "berlin-v6.3.output_persons.csv.gz"
+
+# Download the file
+download.file(url, destfile)
+
+# Get the current working directory (to return later, if needed)
+original_dir <- getwd()
+
+# Move to the directory where the file is located
+file_dir <- dirname(destfile)
+setwd(file_dir)
+
+# Unzip and read the file
+policy_data <- read_delim(gzfile(basename(destfile)))
+
+sample(policy_data)
+
+base_person <- base_data %>% select(person)
+base_person
+
+base_person_berlin <- base_person %>% filter(!grepl("^bb_", person, ignore.case = TRUE))
+base_person_berlin
+
+base_person_bb <- base_person %>% filter(grepl("^bb_", person, ignore.case = TRUE))
+base_person_bb
