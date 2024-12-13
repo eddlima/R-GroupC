863a78415a70dc6be8a7e3412310d82583d10322
Eduardo Lima <eduardo.lima@outlook.de>
2024-12-13 13:22:39 +0100

    Graphic color not possible

--- R-GroupC.R
+++ R-GroupC.R
@@ -6,56 +6,34 @@ library(stringr)
 library(forcats)
 
 #BASE
-# Define the URL of the csv.gz file and the destination file path
 url <- "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/tutorial/datascience2024/matsim_outputs/output-1pct/base/berlin-v6.3.output_trips.csv.gz"
 destfile <- "berlin-v6.3.base.output_trips.csv.gz"
 
-# Download the file
 download.file(url, destfile)
 
-# Get the current working directory (to return later, if needed)
 original_dir <- getwd()
 
-# Move to the directory where the file is located
 file_dir <- dirname(destfile)
 setwd(file_dir)
 
-# Unzip and read the file
 base_data <- read_delim(gzfile(basename(destfile)))
 
 sample(base_data)
 
 #POLICY
-# Define the URL of the csv.gz file and the destination file path
 url <- "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/tutorial/datascience2024/matsim_outputs/output-1pct/policy/berlin-v6.3.output_trips.csv.gz"
 destfile <- "berlin-v6.3.policy.output_trips.csv.gz"
 
-# Download the file
 download.file(url, destfile)
 
-# Get the current working directory (to return later, if needed)
 original_dir <- getwd()
 
-# Move to the directory where the file is located
 file_dir <- dirname(destfile)
 setwd(file_dir)
 
-# Unzip and read the file
 policy_data <- read_delim(gzfile(basename(destfile)))
 
-
-
-base_person <- base_data %>% select(person)
-base_person
-
-base_person_berlin <- base_person %>% filter(!grepl("^bb_", person, ignore.case = TRUE))
-base_person_berlin
-
-base_person_bb <- base_person %>% filter(grepl("^bb_", person, ignore.case = TRUE))
-base_person_bb
-
-fct_count(base_data$main_mode)
-
+# Comparing Mode Share for Brandenburg Residents: Base vs. Policy Scenario
 mode_share_bb_base <- base_data %>% 
   select(trip_id, main_mode) %>% 
   filter(grepl("^bb", trip_id, ignore.case = TRUE)) %>% 
@@ -82,33 +60,36 @@ ggplot(mode_share_bb_combined, aes(x = main_mode, y = share, fill = scenario)) +
   theme_minimal() +
   theme(axis.text.x = element_text(hjust = 1))
 
-
-
-# Comapring travel time
-base_data_bb <- base_data %>% filter(grepl("^bb", trip_id, ignore.case = TRUE))
-policy_data_bb <- policy_data %>% filter(grepl("^bb", trip_id, ignore.case = TRUE))
+# Comparing travel time by main mode
+base_data_bb <- base_data %>% 
+  filter(grepl("^bb", trip_id, ignore.case = TRUE)) %>%
+  mutate(total_trav_time = trav_time + wait_time)  
+policy_data_bb <- policy_data %>% 
+  filter(grepl("^bb", trip_id, ignore.case = TRUE)) %>%
+  mutate(total_trav_time = trav_time + wait_time)
 
 mode_base_summary <- base_data_bb %>% 
   group_by(main_mode) %>% 
-  summarise (avg_travel_time = mean(trav_time, na.rm = TRUE), .ps = 'drop')
+  summarise (avg_travel_time = mean(total_trav_time, na.rm = TRUE), .ps = 'drop')
 
 mode_policy_summary <- policy_data_bb %>% 
   group_by(main_mode) %>% 
-  summarise (avg_travel_time = mean(trav_time, na.rm = TRUE), .ps = 'drop')
+  summarise (avg_travel_time = mean(total_trav_time, na.rm = TRUE), .ps = 'drop')
 
 mode_summary_combined <- left_join(mode_base_summary, mode_policy_summary,
                                    by = "main_mode",
-                                   suffix = c("_base", "_policy")) %>% 
-  arrange((avg_travel_time_base))
+                                   suffix = c("_base", "_policy")) %>%
+  mutate(avg_travel_time_diff = avg_travel_time_policy - avg_travel_time_base)
+  
 
-ggplot(mode_summary_combined, aes(x = main_mode)) +
-  geom_col(aes(y = avg_travel_time_base, fill = "Base"), position = "dodge", alpha = 0.7) +
-  geom_col(aes(y = avg_travel_time_policy, fill = "Policy"), position = "dodge", alpha = 0.5) +
-  labs(title = "Average Travel Time for Brandenburg Residents: Base vs Policy Scenario",
-       x = "Mode of Transportation", y = "Average Travel Time (minutes)") +
-  scale_fill_manual(values = c("Base" = "blue", "Policy" = "red")) +
+ggplot(mode_summary_combined, aes(x = main_mode, y = avg_travel_time_diff)) +
+  geom_col(aes( fill = "Diff"), position = "dodge", alpha = 0.7) +
+  labs(title = "Difference of Average Travel Time for Brandenburg Residents",
+       x = "Mode of Transportation", y = "Difference of Average Travel Time (seconds) - Policy vs. Base Scenario") +
+  scale_fill_manual(Values = c("Diff" = "green")) +
   theme_minimal() +
-  theme(axis.text.x = element_text(hjust = 1, angle = 45))
+  theme(legend.position = "none", legend.title = element_blank()) +
+  theme(axis.text.x = element_text(hjust = 1))
 
 
 
